28a4c07cb6610777ededa3f441e4ee5aebee1102
Ammad <Ammad@DESKTOP-MQ0U50F>
2018-04-28 08:26:45 +0500

    CPTRinitial commmit

---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Data.Model;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public virtual ICollection<Physician> Physicians { get; set; }
+        public virtual ICollection<PatientUser> PatientUsers { get; set; }
+    }
+}
